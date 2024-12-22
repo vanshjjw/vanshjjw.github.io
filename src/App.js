df5da77aa776ff1afaaf6bdf36628d19bd7f9213
Vansh <vanshjjw@gmail.com>
2024-12-23 01:44:15 +0530

    sidebar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Preloader from "./components/General/Preloader";
 import ScrollToTop from "./components/General/ScrollToTop";
 import Sidebar from "./components/General/Sidebar";
 import AboutMe from "./components/About/AboutMe";
-import Homepage from "./components/General/Homepage";
 
 import "./style.css";
 import "./App.css";
@@ -23,31 +22,21 @@ function App() {
 
   return (
       <Router>
-        <Preloader load={load} />
-        <div className="App" id={load ? "no-scroll" : "scroll"}>
-          <ScrollToTop />
-          <Routes>
-            {/* Route for the homepage with expanded sidebar */}
-            <Route path="/" element={<Homepage />} />
-            {/* Route for the app layout with sidebar */}
-            <Route
-                path="/*"
-                element={
-                  <div className="app-layout">
-                    <Sidebar />
-                    <div className="main-content">
+          <Preloader load={load} />
+          <div className="App" id={load ? "no-scroll" : "scroll"}>
+              <ScrollToTop />
+              <div className="app-layout">
+                  <Sidebar />
+                  <div className="main-content">
                       <Routes>
-                        <Route path="/intro" element={<AboutMe />} />
-                        <Route path="/resume" element={<h1>Coming Soon...</h1>} />
-                        <Route path="/blog" element={<h1>Coming Soon...</h1>} />
-                        {/* Add other routes here when additional sections are implemented */}
+                          <Route path="/intro" element={<AboutMe />} />
+                          <Route path="/resume" element={<h1>Coming Soon...</h1>} />
+                          <Route path="/blog" element={<h1>Coming Soon...</h1>} />
+                          {/* Add other routes here when additional sections are implemented */}
                       </Routes>
-                    </div>
                   </div>
-                }
-            />
-          </Routes>
-        </div>
+              </div>
+          </div>
       </Router>
   );
 }
