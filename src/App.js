23a7e04d2d4c3326b16ac21ca2059a11bcbcdeb0
Vansh <vanshjjw@gmail.com>
2024-12-22 22:37:51 +0530

    Project-reload-1

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import Preloader from "../src/components/Pre";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
+import "./style.css";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  const [load, upadateLoad] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      upadateLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <Router>
+      <Preloader load={load} />
+      <div className="App" id={load ? "no-scroll" : "scroll"}>
+        <ScrollToTop />
+
+      </div>
+    </Router>
+  );
+}
+
+export default App;
