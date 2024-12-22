38eb543ef2c6f1d46c56d5048ff3cb88c6770398
Vansh <vanshjjw@gmail.com>
2024-12-23 01:16:59 +0530

    homepage and sidebar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Preloader from "./components/General/Preloader";
 import ScrollToTop from "./components/General/ScrollToTop";
 import Sidebar from "./components/General/Sidebar";
 import AboutMe from "./components/About/AboutMe";
+import Homepage from "./components/General/Homepage";
 
 import "./style.css";
 import "./App.css";
@@ -25,17 +26,27 @@ function App() {
         <Preloader load={load} />
         <div className="App" id={load ? "no-scroll" : "scroll"}>
           <ScrollToTop />
-          <div className="app-layout">
-            <Sidebar />
-            <div className="main-content">
-              <Routes>
-                <Route path="/intro" element={<AboutMe />} />
-                <Route path="/resume" element={<h1>Coming Soon...</h1>} />
-                <Route path="/blog" element={<h1>Coming Soon...</h1>} />
-                {/* Add other routes here when additional sections are implemented */}
-              </Routes>
-            </div>
-          </div>
+          <Routes>
+            {/* Route for the homepage with expanded sidebar */}
+            <Route path="/" element={<Homepage />} />
+            {/* Route for the app layout with sidebar */}
+            <Route
+                path="/*"
+                element={
+                  <div className="app-layout">
+                    <Sidebar />
+                    <div className="main-content">
+                      <Routes>
+                        <Route path="/intro" element={<AboutMe />} />
+                        <Route path="/resume" element={<h1>Coming Soon...</h1>} />
+                        <Route path="/blog" element={<h1>Coming Soon...</h1>} />
+                        {/* Add other routes here when additional sections are implemented */}
+                      </Routes>
+                    </div>
+                  </div>
+                }
+            />
+          </Routes>
         </div>
       </Router>
   );
--- src/components/General/Homepage.js
+++ src/components/General/Homepage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Siderbar.css';
 
-const HomePage = () => {
+const Homepage = () => {
     return (
         <div className="homepage">
             <div className="sidebar homepage-sidebar">
@@ -20,4 +20,4 @@ const HomePage = () => {
     );
 };
 
-export default HomePage;
+export default Homepage;
