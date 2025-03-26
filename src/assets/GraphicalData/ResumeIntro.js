fa09d093fe7b24efb1512297fe62d92b7d6cb758
Vansh <vanshjjw@gmail.com>
2025-03-26 13:14:45 +0530

    nice

---
+++ src/assets/GraphicalData/ResumeIntro.js
@@ -0,0 +1,10 @@
+// Introduction text for the Resume page
+export const RESUME_INTRODUCTION = `
+This interactive resume visualizes the connections between my **skills** and **experiences**. 
+
+Select a primary skill category to see my sub-skills. Click on a sub-skill to highlight related experiences. Click again to see tags associated with that skill.
+
+*You can also click on experience cards to read more details.*
+`;
+
+export default RESUME_INTRODUCTION; 
